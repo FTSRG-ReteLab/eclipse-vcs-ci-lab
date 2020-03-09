a2cbbbf369d5f458fa00b9f3d5c2aa889005a99a
xVictoryerx <toth9viktor9@gmail.com>
2020-03-09 15:51:39 +0100

    Extra layer of speed limitation added.

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -42,6 +42,9 @@ public class TrainControllerImpl implements TrainController {
 		if (referenceSpeed > speedLimit - 2) {
 			System.out.println("Approaching speed limit!");
 		}
+		if (referenceSpeed z speedLimit - 3) {
+			System.out.println("You are going fast");
+		}
 	}
 
 	@Override
